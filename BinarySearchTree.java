7489b384dfac3d536fe0e0af545b538e4116e468
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2010-04-11 22:29:56 -0300

    Basic stuff

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,43 +1,69 @@
 public class BinarySearchTree<K extends Comparable<K>, T> {
-    public bool insertRecursive(BinarySearchTreeNode<K, T> node, key, T obj) {
-	if (key.compareTo(node.getKey()) == 0) {
-	    return false;
+	private BinarySearchTreeNode<K, T> root;
+
+	public BinarySearchTree() {
+		root = new BinarySearchTreeNode();
 	}
-	else {
-	    boolean inseriu = false;
-	    if (key.compareTo(node.getKey()) < 0) {
-		inseriu = this.insertRecursive(node.getLeft(), key, obj);
-	    } else if (key.compareTo(node.getKey()) > 0) {
-		inseriu = this.insertRecursive(node.getRight(), key, obj);
-	    }
-	    if (inseriu) {
-		node.adjustHeight();
-	    }
-	    if (Math.abs(node.getBalanceFactor()) > 1) {
-		this.balance(node);
-	    }
-	    return inseriu;
+
+	public boolean isEmpty() {
+		return this.root.isTerminal();
 	}
-    }
-    
-    public void balance(AVLSearchTreeNode<K, T> node) {
+	public boolean insert(K key, T obj) {
+		return insertRecursive(this.root, key, obj);
+	}
+	//public boolean remove(K key){ ... }
+	//public T search(K key){ ... }
+
+	public bool insertRecursive(BinarySearchTreeNode<K, T> node, K key, T obj) {
+		if (node.isTerminal()) {
+			node.setKey(key);
+			node.setObject(aluno);
+
+			node.left = new BinarySearchTreeNode();
+			node.right = new BinarySearchTreeNode();
+
+			return true;
+		}
+		else if (key.compareTo(node.getKey()) == 0) {
+			return false;
+		}
+		else {
+			boolean inseriu = false;
+			if (key.compareTo(node.getKey()) < 0) {
+				inseriu = this.insertRecursive(node.getLeft(), key, obj);
+			}
+			else if (key.compareTo(node.getKey()) > 0) {
+				inseriu = this.insertRecursive(node.getRight(), key, obj);
+			}
+
+			if (inseriu) {
+				node.adjustHeight();
+			}
+			if (Math.abs(node.getBalanceFactor()) > 1) {
+				this.balance(node);
+			}
+			return inseriu;
+		}
+	}
+	
+	public void balance(AVLSearchTreeNode<K, T> node) {
 	if (node.getLeft().getHeight() > node.getRight().getHeight()) {
-	    if (node.getLeft().getLeft().getHeight() >
-                node.getLeft().getRight().getHeight()) {
+		if (node.getLeft().getLeft().getHeight() >
+		node.getLeft().getRight().getHeight()) {
 		this.rotateRight(node);
-	    }
-	    else {
+		}
+		else {
 		this.rotateDoubleRight(node);
-	    }
+		}
 	}
 	else {
-	    if (node.getRight().getRight().getHeight() >
+		if (node.getRight().getRight().getHeight() >
 		node.getRight().getLeft().getHeight()) {
 		this.rotateLeft(node);
-	    }
-	    else {
+		}
+		else {
 		this.rotateDoubleLeft(node);
-	    }
+		}
 	}
-    }
+	}
 }
--- BinarySearchTreeNode.java
+++ BinarySearchTreeNode.java
@@ -1,21 +1,42 @@
 
 public class BinarySearchTreeNode<K extends Comparable<K>, T> {
-    private K key;
-    private T object;
-    private BinarySearchTreeNode<K, T> right;
-    private BinarySearchTreeNode<K, T> left;
-    private int height;
-    public int getBalanceFactor() {
-	return this.getLeft().getHeight() -
-	    this.getRight().getHeight();
-    }
-    public void adjustHeight() {
-	if (this.getLeft().getHeight() >
-	    this.getRight().getHeight()){
-	    this.height = this.getLeft().getHeight() + 1;
+	private K key;
+	private T object;
+	private BinarySearchTreeNode<K, T> right;
+	private BinarySearchTreeNode<K, T> left;
+	private int height;
+
+	public BinarySearchTreeNode() {
+		height = 0;
 	}
-	else {
-	    this.height = this.getRight().getHeight() + 1;
+
+	public int getBalanceFactor() {
+		return this.getLeft().getHeight() -
+			this.getRight().getHeight();
 	}
+	public void adjustHeight() {
+		if (this.getLeft().getHeight() >
+			this.getRight().getHeight()){
+			this.height = this.getLeft().getHeight() + 1;
+		}
+		else {
+			this.height = this.getRight().getHeight() + 1;
+		}
    }
+
+	public boolean isTerminal(){
+		return (this.right == null && this.left == null);
+	}
+	public K getKey() {
+		return key;
+	}
+	public T getObject() {
+		return T;
+	}
+	public BinarySearchTreeNode<K, T> getRight() {
+		return right;
+	}
+	public BinarySearchTreeNode<K, T> getLeft() {
+		return left;
+	}
 }
